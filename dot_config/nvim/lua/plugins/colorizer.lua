385ed82c20688a9da774c5b685196de5b5ccf554
redbeardymcgee <redbeardymcgee@gmail.com>
2024-04-29 20:40:31 -0500

    missing files

---
+++ dot_config/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,15 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  opts = {
+    user_default_options = {
+      tailwind = true,
+      css = true,
+    },
+  },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    opts = {
+      color_square_width = 2,
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/typescript.lua
@@ -0,0 +1,54 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    inlay_hints = {
+      enabled = true,
+    },
+    codelens = {
+      enabled = true,
+    },
+  },
+  servers = {
+    tsserver = {
+      settings = {
+        typescript = {
+          inlayHints = {
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+          },
+        },
+        javascript = {
+          inlayHints = {
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+          },
+        },
+      },
+    },
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      settings = {
+        expose_as_code_action = "all",
+        jsx_close_tag = {
+          enable = true,
+          filetypes = { "javascriptreact", "typescriptreact" },
+        },
+      },
+    },
+  },
+}
