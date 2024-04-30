f73c81a8ed3d3c92df595b9c164845e675714dba
redbeardymcgee <redbeardymcgee@gmail.com>
2024-04-29 20:13:38 -0500

    fixx themee

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
   {
     "catppuccin",
-    -- opts = {
-    --   transparent_background = true,
-    -- },
+    opts = {
+      transparent_background = true,
+    },
   },
   {
     "LazyVim/LazyVim",
