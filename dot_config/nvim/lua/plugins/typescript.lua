e5c0ac566277d6bf08e798bee48b05529d8d09a0
redbeardymcgee <redbeardymcgee@gmail.com>
2024-04-30 15:23:04 -0500

    Update .config/nvim/lua/plugins/typescript.lua

--- dot_config/nvim/lua/plugins/typescript.lua
+++ dot_config/nvim/lua/plugins/typescript.lua
@@ -13,23 +13,10 @@ return {
       "windwp/nvim-ts-autotag",
       cond = false,
     },
-    -- {
-    --   "williamboman/mason-lspconfig.nvim",
-    --   opts = {
-    --     handlers = {
-    --       function(server_name)
-    --         if server_name == "tsserver" then
-    --           return
-    --         end
-    --       end,
-    --     },
-    --   },
-    -- },
     {
       "pmizio/typescript-tools.nvim",
       dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
       opts = {
-        -- cmd = { "typescript-language-server", "--stdio" },
         settings = {
           expose_as_code_action = "all",
           jsx_close_tag = {
