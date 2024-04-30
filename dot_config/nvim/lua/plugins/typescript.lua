e84fa6520f80310b324a268699c4db2541800296
redbeardymcgee <redbeardymcgee@gmail.com>
2024-04-30 13:26:35 -0500

    Update .config/nvim/lua/plugins/typescript.lua

--- dot_config/nvim/lua/plugins/typescript.lua
+++ dot_config/nvim/lua/plugins/typescript.lua
@@ -8,9 +8,16 @@ return {
       codelens = {
         enabled = true,
       },
-      servers = {
-        tsserver = {
-          cond = false,
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        handlers = {
+          function(server_name)
+            if server_name == "tsserver" then
+              return
+            end
+          end,
         },
       },
     },
