8f51b48b0b6590a5c14f69b028254b155f3e1b49
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-01 00:51:03 -0500

    Update .bashrc Add .config/ble.rc Update .config/distrobox/dev.ini Add .config/wezterm/wezterm.lua Update .local/share/applications/dev.desktop

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,54 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.harfbuzz_features = {
+	"zero",
+	"cv17",
+	"ss05",
+	"cv16",
+	"cv31",
+	"cv29",
+	"cv30",
+	"cv23",
+	"ss08",
+	"ss09",
+	"cv25",
+	"cv26",
+	"cv32",
+	"cv27",
+	"cv28",
+	"ss06",
+	"ss07",
+}
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.ssh_domains = {
+	{
+		name = "",
+		remote_address = "media.mcgee.network",
+		username = "josh",
+	},
+}
+
+wezterm.on("update-status", function(window, pane)
+	local meta = pane:get_metadata() or {}
+	if meta.is_tardy then
+		local secs = meta.since_last_response_ms / 1000.0
+		window:set_right_status(string.format("tardy: %5.1fs⏳", secs))
+	end
+end)
+
+wezterm.on("update-status", function(window, pane)
+	local meta = pane:get_metadata() or {}
+	local overrides = window:get_config_overrides() or {}
+	if meta.password_input then
+		overrides.color_scheme = "Red Alert"
+	else
+		overrides.color_scheme = nil
+	end
+	window:set_config_overrides(overrides)
+end)
+
+return config
