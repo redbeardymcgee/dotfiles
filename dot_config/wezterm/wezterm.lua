0755105fd875ad08965838ae1ff2b06b89c9066b
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-04 20:29:15 -0500

    Remove .config/wezterm/keys.lua Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,34 +3,47 @@ local config = wezterm.config_builder()
 local act = wezterm.action
 local kb = require("keys")
 
--- local function activate_pane(window, pane, pane_direction, vim_direction)
--- 	local isViProcess = pane:get_foreground_process_name():find("n?vim") ~= nil
--- 	if isViProcess then
--- 		window:perform_action( -- This should match the keybinds you set in Neovim.
--- 			act.SendKey({ key = vim_direction, mods = "CTRL" }),
--- 			pane
--- 		)
--- 	else
--- 		window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
--- 	end
--- end
+local function activate_pane(window, pane, pane_direction, vim_direction)
+	local isViProcess = pane:get_foreground_process_name():find("n?vim") ~= nil
+	if isViProcess then
+		window:perform_action( -- This should match the keybinds you set in Neovim.
+			act.SendKey({ key = vim_direction, mods = "CTRL" }),
+			pane
+		)
+	else
+		window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+	end
+end
 
--- wezterm.on("activate_pane_r", function(window, pane)
--- 	activate_pane(window, pane, "Right", "l")
--- end)
--- wezterm.on("activate_pane_l", function(window, pane)
--- 	activate_pane(window, pane, "Left", "h")
--- end)
--- wezterm.on("activate_pane_u", function(window, pane)
--- 	activate_pane(window, pane, "Up", "k")
--- end)
--- wezterm.on("activate_pane_d", function(window, pane)
--- 	activate_pane(window, pane, "Down", "j")
--- end)
+wezterm.on("activate_pane_r", function(window, pane)
+	activate_pane(window, pane, "Right", "l")
+end)
+wezterm.on("activate_pane_l", function(window, pane)
+	activate_pane(window, pane, "Left", "h")
+end)
+wezterm.on("activate_pane_u", function(window, pane)
+	activate_pane(window, pane, "Up", "k")
+end)
+wezterm.on("activate_pane_d", function(window, pane)
+	activate_pane(window, pane, "Down", "j")
+end)
 
-config.disable_default_key_bindings = true
-config.keys = kb.keys
-config.key_tables = kb.key_tables
+config.keys = {
+	-- 	{ key = "h", mods = "CTRL", action = act.DisableDefaultAssignment },
+	-- 	{ key = "j", mods = "CTRL", action = act.DisableDefaultAssignment },
+	-- 	{ key = "k", mods = "CTRL", action = act.DisableDefaultAssignment },
+	-- 	{ key = "l", mods = "CTRL", action = act.DisableDefaultAssignment },
+	--
+	{ key = "h", mods = "CTRL", action = act.EmitEvent("activate_pane_h") },
+	{ key = "j", mods = "CTRL", action = act.EmitEvent("activate_pane_h") },
+	{ key = "k", mods = "CTRL", action = act.EmitEvent("activate_pane_h") },
+	{ key = "l", mods = "CTRL", action = act.EmitEvent("activate_pane_h") },
+	--
+	-- 	{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+	-- 	{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+	-- 	{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+	-- 	{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+}
 
 config.scrollback_lines = 3500
 
