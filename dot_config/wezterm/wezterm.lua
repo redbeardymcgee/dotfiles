9447d462812813bf2154b800f94fb504f3b6e3f9
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-04 19:31:27 -0500

    Add .config/wezterm/keys.lua Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,40 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local vi_copy_mode = require("vi-copy-mode")
+local act = wezterm.action
+
+local function activate_pane(window, pane, pane_direction, vim_direction)
+	local isViProcess = pane:get_foreground_process_name():find("n?vim") ~= nil
+	if isViProcess then
+		window:perform_action( -- This should match the keybinds you set in Neovim.
+			act.SendKey({ key = vim_direction, mods = "CTRL" }),
+			pane
+		)
+	else
+		window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+	end
+end
+
+wezterm.on("activate_pane_r", function(window, pane)
+	activate_pane(window, pane, "Right", "l")
+end)
+wezterm.on("activate_pane_l", function(window, pane)
+	activate_pane(window, pane, "Left", "h")
+end)
+wezterm.on("activate_pane_u", function(window, pane)
+	activate_pane(window, pane, "Up", "k")
+end)
+wezterm.on("activate_pane_d", function(window, pane)
+	activate_pane(window, pane, "Down", "j")
+end)
+
+config.disable_default_key_bindings = true
+config.keys = {
+	{ key = "h", mods = "CTRL", action = act.EmitEvent("activate_pane_l") },
+	{ key = "j", mods = "CTRL", action = act.EmitEvent("activate_pane_d") },
+	{ key = "k", mods = "CTRL", action = act.EmitEvent("activate_pane_u") },
+	{ key = "l", mods = "CTRL", action = act.EmitEvent("activate_pane_r") },
+}
 
 config.scrollback_lines = 3500
 
@@ -31,8 +66,12 @@ config.ssh_domains = {
 	{
 		name = "mcgeedia",
 		remote_address = "media.mcgee.network",
-		-- remote_wezterm_path = "/home/josh/devbox/.local/bin/wezterm",
 	},
+}
+
+config.inactive_pane_hsb = {
+	saturation = 0.3,
+	brightness = 0.6,
 }
 
 wezterm.on("update-status", function(window, pane)
