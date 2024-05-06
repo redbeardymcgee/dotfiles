8ede242c4bcd9a71f65b392d722ac6e13450d4ef
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-05 20:53:55 -0500

    Update .local/lib/rbm/utils.sh

--- dot_local/lib/rbm/utils.sh
+++ dot_local/lib/rbm/utils.sh
@@ -11,3 +11,17 @@ prepend_to_PATH() {
 		fi # skip nonexistent directory
 	done
 }
+
+pathmunge() {
+	d=$({ cd -- "$1" && { pwd -P || pwd; }; } 2>/dev/null) # canonicalize symbolic links
+	case ":${PATH}:" in
+	*:"$d":*) ;;
+	*)
+		if [ "$2" = "after" ]; then
+			PATH=$PATH:$d
+		else
+			PATH=$d:$PATH
+		fi
+		;;
+	esac
+}
