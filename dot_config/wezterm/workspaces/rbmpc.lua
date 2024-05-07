242cdb54f5093964c3247bb0b346c2ef4803d25b
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-07 18:42:12 -0500

    Update .config/wezterm/workspaces/rbmpc.lua

--- dot_config/wezterm/workspaces/rbmpc.lua
+++ dot_config/wezterm/workspaces/rbmpc.lua
@@ -4,15 +4,15 @@ function M.startup(wezterm, workspace_name)
 	local mux = wezterm.mux
 	local config_dir = wezterm.home_dir .. ".config"
 
-	local df_tab, df_pane, df_window = mux.spawn_window({
+	local dotfiles_tab, dotfiles_pane, dotfiles_window = mux.spawn_window({
 		workspace = workspace_name,
 		cwd = config_dir,
 		args = { "nvim-nightly", "." },
 	})
 
-	df_tab:set_title("dotfiles")
+	dotfiles_tab:set_title("dotfiles")
 
-	df_pane:split({ direction = "Right" })
+	dotfiles_pane:split({ direction = "Right" })
 
-	df_pane:activate()
+	dotfiles_pane:activate()
 end
