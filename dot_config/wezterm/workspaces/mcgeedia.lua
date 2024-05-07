71cad717c7d7c9351d6943a300faec6f1bb49f04
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-07 17:06:32 -0500

    Update .config/wezterm/workspaces/mcgeedia.lua

--- dot_config/wezterm/workspaces/mcgeedia.lua
+++ dot_config/wezterm/workspaces/mcgeedia.lua
@@ -4,32 +4,22 @@ function M.startup(wezterm)
 	local mux = wezterm.mux
 	local containers_dir = "/opt/containers"
 	local workspace_name = "McGeedia"
-	local domain_name = "mcgeedia"
+
+	mux.set_default_domain("mcgeedia")
 
 	local mcgeedia_tab, mcgeedia_pane, mcgeedia_window = mux.spawn_window({
-		domain = {
-			DomainName = domain_name,
-		},
+		cwd = containers_dir,
 		workspace = workspace_name,
 	})
 
 	mcgeedia_window:set_title("McGeedia")
 	mcgeedia_tab:set_title("Shell")
 
-	local containers_tab, containers_pane, _ = mcgeedia_window:spawn_tab({
-		domain = {
-			DomainName = domain_name,
-		},
-		workspace = workspace_name,
-		cwd = containers_dir,
-	})
+	local containers_tab, containers_pane, containers_window = mcgeedia_window:spawn_tab()
 
 	containers_tab:set_title("Containers")
 
 	containers_pane:split({
-		domain = {
-			DomainName = domain_name,
-		},
 		{ direction = "Right", args = { "nvim-nightly", "." } },
 	})
 
