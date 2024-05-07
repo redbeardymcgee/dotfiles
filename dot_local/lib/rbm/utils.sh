01ed491f46551c6371317986b101e6e94773d3b8
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-07 13:59:55 -0500

    Update .bashrc Update .local/bin/devbox.bash Update .local/lib/rbm/utils.sh Update .profile

--- dot_local/lib/rbm/utils.sh
+++ dot_local/lib/rbm/utils.sh
@@ -1,17 +1,3 @@
-prepend_to_PATH() {
-	for d; do
-		d=$({ cd -- "$d" && { pwd -P || pwd; }; } 2>/dev/null) # canonicalize symbolic links
-		if [ -d "$d" ]; then
-			case ":$PATH:" in
-			*":$d:"*) : ;;
-			*)
-				PATH="$d:$PATH"
-				;;
-			esac
-		fi # skip nonexistent directory
-	done
-}
-
 pathmunge() {
 	d=$({ cd -- "$1" && { pwd -P || pwd; }; } 2>/dev/null) # canonicalize symbolic links
 	case ":${PATH}:" in
