6ebc3894b6c9cc8859b09f3d688cb3dbc275c7c2
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-07 00:44:44 -0500

    Update .config/yazi/init.lua

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -49,20 +49,20 @@ end
 -- 		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
 -- 	}
 -- end
---
--- function Status:owner()
--- 	local h = cx.active.current.hovered
--- 	if h == nil or ya.target_family() ~= "unix" then
--- 		return ui.Line({})
--- 	end
---
--- 	return ui.Line({
--- 		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
--- 		ui.Span(":"),
--- 		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
--- 		ui.Span(" "),
--- 	})
--- end
+
+function Status:owner()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	})
+end
 
 function Status:render(area)
 	self.area = area
