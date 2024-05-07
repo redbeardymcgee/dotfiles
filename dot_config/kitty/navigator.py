83a0fd75a8735ee18f5942d488c1cb3e371ce3cd
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-07 15:15:57 -0500

    Update .config/kitty/navigator.py

--- dot_config/kitty/navigator.py
+++ dot_config/kitty/navigator.py
@@ -3,10 +3,7 @@ def main():
 
 
 def handle_result(args, result, target_window_id, boss):
-    if args[1] == "p":
-        boss.active_tab.nth_window(-1)
-    else:
-        boss.active_tab.neighboring_window(args[1])
+    return boss.active_tab.current_layout.name 
 
 
 handle_result.no_ui = True
