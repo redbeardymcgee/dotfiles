d085a534879a02ffe31264c29786109ef01e733e
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-08 18:30:47 -0500

    Update .local/lib/rbm/utils.sh

--- dot_local/lib/rbm/utils.sh
+++ dot_local/lib/rbm/utils.sh
@@ -1,18 +1,13 @@
-UTILS_SOURCED=${UTILS_SOURCED:-false}
-if [[ $UTILS_SOURCED = "false" ]]; then
-	pathmunge() {
-		d=$({ cd -- "$1" && { pwd -P || pwd; }; } 2>/dev/null) # canonicalize symbolic links
-		case ":${PATH}:" in
-		*:"$d":*) ;;
-		*)
-			if [ "$2" = "after" ]; then
-				PATH=$PATH:$d
-			else
-				PATH=$d:$PATH
-			fi
-			;;
-		esac
-	}
-fi
-
-export UTILS_SOURCED=true
+pathmunge() {
+	d=$({ cd -- "$1" && { pwd -P || pwd; }; } 2>/dev/null) # canonicalize symbolic links
+	case ":${PATH}:" in
+	*:"$d":*) ;;
+	*)
+		if [ "$2" = "after" ]; then
+			PATH=$PATH:$d
+		else
+			PATH=$d:$PATH
+		fi
+		;;
+	esac
+}
