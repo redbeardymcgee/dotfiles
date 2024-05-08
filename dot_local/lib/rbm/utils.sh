7e5d77adc55e590ee68cbb731681fa7672bc2e4f
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-08 00:13:15 -0500

    Update .bashrc Update .config/bat/config Update .config/zsh/.aliases Update .config/zsh/.zshrc Add .config/zsh/functions/batman Update .local/bin/after_executable_devbox.bash Update .local/lib/rbm/utils.sh Update .profile Remove .zshenv Add .zshenv Change attributes of .config/zsh/.zprofile

--- dot_local/lib/rbm/utils.sh
+++ dot_local/lib/rbm/utils.sh
@@ -1,18 +1,19 @@
 UTILS_SOURCED=${UTILS_SOURCED:-false}
-if [[ $UTILS_SOURCED = "false" ]]; then
-	pathmunge() {
-		d=$({ cd -- "$1" && { pwd -P || pwd; }; } 2>/dev/null) # canonicalize symbolic links
-		case ":${PATH}:" in
-		*:"$d":*) ;;
-		*)
-			if [ "$2" = "after" ]; then
-				PATH=$PATH:$d
-			else
-				PATH=$d:$PATH
-			fi
-			;;
-		esac
-	}
+if [[ $UTILS_SOURCED = "true" ]]; then
+	return 1
 fi
+pathmunge() {
+	d=$({ cd -- "$1" && { pwd -P || pwd; }; } 2>/dev/null) # canonicalize symbolic links
+	case ":${PATH}:" in
+	*:"$d":*) ;;
+	*)
+		if [ "$2" = "after" ]; then
+			PATH=$PATH:$d
+		else
+			PATH=$d:$PATH
+		fi
+		;;
+	esac
+}
 
 export UTILS_SOURCED=true
