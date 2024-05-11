bc22e7116ff556ba99f6c52abbdb768fa4237795
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-11 15:41:15 -0500

    Add .config/nvim/lua/plugins/copilot.lua

---
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = { enabled = true, auto_trigger = true },
+    panel = { enabled = false },
+    filetypes = {
+      help = true,
+      markdown = true,
+      typescript = true,
+      toml = true,
+      yaml = true,
+    },
+  },
+}
