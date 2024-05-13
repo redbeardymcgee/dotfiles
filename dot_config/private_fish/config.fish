c9da356fa91c612c4788b66d8f966d835249abc7
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-13 02:57:01 -0500

    Update .config/fish/config.fish Remove .config/fish/fish_plugins

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,12 +1,15 @@
 if status is-interactive
 
+    abbr -a cat bat
+
     abbr -a gp gtrash put
     abbr -a rm echo "Use `gp` (`gtrash put`)"
     abbr -a vim nvim
 
     devbox global shellenv | source
-    starship init fish | source
+
     atuin init fish | source
-    zoxide init fish --cmd cd | source
+    starship init fish | source
     direnv hook fish | source
+    zoxide init fish --cmd cd | source
 end
