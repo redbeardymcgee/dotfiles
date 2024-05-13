4556d73462caf06fb47983d589eb0cd178809891
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-13 17:53:39 -0500

    Remove config.fish.bak Update .config/fish/config.fish Remove xrun_once_setup.sh

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,12 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    devbox global shellenv --init-hook | source
+    devbox global shellenv | source
+
+    abbr -a gp gtrash put
+    abbr -a rm echo "Use `gp` (`gtrash put`)"
+    abbr -a vim nvim
+
+    starship init fish | source
+    atuin init fish | source
+    zoxide init fish --cmd cd | source
+    direnv hook fish | source
 end
--- xrun_once_setup.sh
+++
@@ -1,8 +0,0 @@
-setup_completions() {
-	for shell in bash fish zsh; do
-		out_dir=${XDG_DATA_HOME:-$HOME/.local/share}/$shell/completion.d
-
-		atuin gen-completions --shell $shell --out-dir "$out_dir"
-
-	done
-}
