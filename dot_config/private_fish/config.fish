cc87ef19a1f717d9d6a37ccc29ab703c0cb5da35
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-13 14:54:04 -0500

    Update .config/fish/config.fish Update .config/fish/fish_plugins Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,15 +1,4 @@
 if status is-interactive
-
-    abbr -a cat bat
-
-    abbr -a gp gtrash put
-    abbr -a rm echo "Use `gp` (`gtrash put`)"
-    abbr -a vim nvim
-
-    devbox global shellenv | source
-
-    atuin init fish | source
-    starship init fish | source
-    direnv hook fish | source
-    zoxide init fish --cmd cd | source
+    # Commands to run in interactive sessions can go here
+    devbox global shellenv --init-hook | source
 end
