97ff9fa93aca2b61bf6e00db97ad9e350d6e3a95
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-13 01:04:08 -0500

    Add .config/fish/completions/.keep Add .config/fish/conf.d/.keep Add .config/fish/config.fish Add .config/fish/fish_variables Add .config/fish/functions/fundle.fish Update setup.sh

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,4 +1,4 @@
-setup_atuin() {
+setup_completions() {
 	for shell in bash fish zsh; do
 		out_dir=${XDG_DATA_HOME:-$HOME/.local/share}/$shell/completion.d
 		atuin gen-completions --shell $shell --out-dir "$out_dir"
