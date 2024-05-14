53d52b4a53d75a9b835b01dc65f505c39af8c907
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-13 19:45:49 -0500

    Add .config/nvim/lua/plugins/nushell.lua

---
+++ dot_config/nvim/lua/plugins/nushell.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  config = function()
+    -- setup treesitter with config
+  end,
+  dependencies = {
+    -- NOTE: additional parser
+    { "nushell/tree-sitter-nu" },
+  },
+  build = ":TSUpdate",
+}
