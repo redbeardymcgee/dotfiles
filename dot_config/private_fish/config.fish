0e0df68e190bc34130cf1f36845746ea430e52c9
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-15 13:38:49 -0500

    Update .config/fish/config.fish Update .config/fish/functions/yy.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -8,6 +8,8 @@ if status is-interactive
     set -gx EDITOR nvim
     set -gx VISUAL nvim
 
+    set -gx NODE_PATH ~/.local/lib/node_modules
+
     starship init fish | source
     atuin init fish | source
     zoxide init fish --cmd cd | source
