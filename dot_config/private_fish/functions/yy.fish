9fb8c148bb76c0d86785b122e3844b3a931b1cff
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-15 19:10:56 -0500

    Update .config/fish/functions/yy.fish

--- dot_config/private_fish/functions/yy.fish
+++ dot_config/private_fish/functions/yy.fish
@@ -1,5 +1,5 @@
 function yy
-    set tmp (mktemp -p $XDG_RUNTIME_DIR/yazi/yazi-cwd.XXXXXX)
+    set tmp (mktemp -p $XDG_RUNTIME_DIR/yazi-cwd.XXXXXX)
     yazi $argv --cwd-file="$tmp"
     if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
         cd -- "$cwd"
