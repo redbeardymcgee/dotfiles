db4060330a44a12f4c70fff92d2f97d3bbb30323
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-20 15:26:12 -0500

    Update .config/nvim/lua/plugins/copilot.lua

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -4,13 +4,13 @@ return {
   opts = {
     suggestion = { enabled = false },
     panel = { enabled = false },
-    filetypes = {
-      help = true,
-      markdown = true,
-      typescript = true,
-      typescriptreact = true,
-      toml = true,
-      yaml = true,
-    },
+    -- filetypes = {
+    --   help = true,
+    --   markdown = true,
+    --   typescript = true,
+    --   typescriptreact = true,
+    --   toml = true,
+    --   yaml = true,
+    -- },
   },
 }
