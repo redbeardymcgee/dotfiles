497cbcf276c6edc1a9da3bd768772fd69d3a7cd3
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-22 21:06:26 -0500

    Update .config/nvim/lua/plugins/racket.lua

--- dot_config/nvim/lua/plugins/racket.lua
+++ dot_config/nvim/lua/plugins/racket.lua
@@ -1,4 +1,19 @@
 return {
-  { "wlangstroth/vim-racket" },
-  { "Olical/conjure" },
+  {
+    "wlangstroth/vim-racket",
+    dependencies = {
+      { "Olical/conjure" },
+      { "PaterJason/cmp-conjure" },
+      {
+        "nvim-cmp",
+        opts = function(_, opts)
+          table.insert(opts.sources, 1, {
+            name = "conjure",
+            group_index = 1,
+            priority = 100,
+          })
+        end,
+      },
+    },
+  },
 }
