ee7738d53e5542e94ee32a0db15550270f9e2380
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-25 19:58:01 -0500

    Add .config/nvim/lua/plugins/lspconfig.lua Update .config/nvim/lua/plugins/typescript.lua

--- dot_config/nvim/lua/plugins/typescript.lua
+++ dot_config/nvim/lua/plugins/typescript.lua
@@ -1,55 +1,53 @@
 return {
   {
-    {
-      "pmizio/typescript-tools.nvim",
-      dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-      opts = {
-        settings = {
-          expose_as_code_action = "all",
-          jsx_close_tag = {
-            enable = true,
-            filetypes = { "javascriptreact", "typescriptreact" },
-          },
-          tsserver_file_preferences = {
-            includeInlayParameterNameHints = "all",
-            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayVariableTypeHints = true,
-            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayEnumMemberValueHints = true,
-          },
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      settings = {
+        expose_as_code_action = "all",
+        jsx_close_tag = {
+          enable = true,
+          filetypes = { "javascriptreact", "typescriptreact" },
+        },
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
         },
       },
-      config = function(opts)
-        require("lazyvim.util.lsp").on_attach(function(client, bufnr)
-          local keymap = vim.keymap.set
-          if client.name == "typescript-tools" then
-            keymap(
-              "n",
-              "<leader>cia",
-              "<cmd>TSToolsAddMissingImports<cr>",
-              { buffer = bufnr, desc = "Add Missing Imports" }
-            )
-            keymap("n", "<leader>cir", "<cmd>TSToolsRemoveUnused<cr>", { buffer = bufnr, desc = "Removed Unused" })
-            keymap(
-              "n",
-              "<leader>lr",
-              "<cmd>TSToolsRemoveUnusedImports<cr>",
-              { buffer = bufnr, desc = "Removed Unused Imports" }
-            )
-            keymap(
-              "n",
-              "<leader>cD",
-              "<cmd>TSToolsGoToSourceDefinition<cr>",
-              { buffer = bufnr, desc = "Go To Source Definition" }
-            )
-            keymap("n", "<leader>cX", "<cmd>TSToolsFixAll<cr>", { buffer = bufnr, desc = "Fix All" })
-          end
-        end)
-        require("typescript-tools").setup(opts)
-      end,
     },
+    config = function(opts)
+      require("lazyvim.util.lsp").on_attach(function(client, bufnr)
+        local keymap = vim.keymap.set
+        if client.name == "typescript-tools" then
+          keymap(
+            "n",
+            "<leader>cia",
+            "<cmd>TSToolsAddMissingImports<cr>",
+            { buffer = bufnr, desc = "Add Missing Imports" }
+          )
+          keymap("n", "<leader>cir", "<cmd>TSToolsRemoveUnused<cr>", { buffer = bufnr, desc = "Removed Unused" })
+          keymap(
+            "n",
+            "<leader>lr",
+            "<cmd>TSToolsRemoveUnusedImports<cr>",
+            { buffer = bufnr, desc = "Removed Unused Imports" }
+          )
+          keymap(
+            "n",
+            "<leader>cD",
+            "<cmd>TSToolsGoToSourceDefinition<cr>",
+            { buffer = bufnr, desc = "Go To Source Definition" }
+          )
+          keymap("n", "<leader>cX", "<cmd>TSToolsFixAll<cr>", { buffer = bufnr, desc = "Fix All" })
+        end
+      end)
+      require("typescript-tools").setup(opts)
+    end,
   },
 }
