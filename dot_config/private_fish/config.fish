9e60cd8e47a33560f5b2b98a21bd0beae347a471
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-27 13:09:41 -0500

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,21 +1,21 @@
 if status is-interactive
-    # devbox global shellenv | source
-    #
-    # fish_add_path -gPm ~/.local/lib/node_modules/bin
-    # fish_add_path -gPm ~/.cargo/bin
-    # fish_add_path -gPm ~/.local/bin
+    devbox global shellenv | source
+
+    fish_add_path -gPm ~/.local/lib/node_modules/bin
+    fish_add_path -gPm ~/.cargo/bin
+    fish_add_path -gPm ~/.local/bin
 
     abbr -a gp gtrash put
     abbr -a rm gtrash put
-    # abbr -a vim nvim
+    abbr -a vim nvim
 
-    # set -gx EDITOR nvim
-    # set -gx VISUAL nvim
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
 
     set -gx NODE_PATH ~/.local/lib/node_modules
 
     starship init fish | source
     atuin init fish | source
     zoxide init fish --cmd cd | source
-    # direnv hook fish | source
+    direnv hook fish | source
 end
