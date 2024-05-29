f8f72d30b10780689c2a9b5047d4f871062f443a
redbeardymcgee <redbeardymcgee@gmail.com>
2024-05-29 18:07:44 -0500

    Add .config/nvim/lua/plugins/zk.lua Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -14,6 +14,8 @@ if status is-interactive
 
     set -gx NODE_PATH ~/.local/lib/node_modules
 
+    set -gx ZK_NOTEBOOK_DIR ~/Documents/Notes
+
     starship init fish | source
     atuin init fish | source
     zoxide init fish --cmd cd | source
